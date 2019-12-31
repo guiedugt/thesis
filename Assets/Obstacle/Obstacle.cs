94178081526e53337d1555978714e12fff614c6f
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2019-12-30 22:32:37 -0500

    Initial commit

---
+++ Assets/Obstacle/Obstacle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] Collider ground;
+
+    RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool hitSomething = Physics.Raycast(transform.position, Vector3.down, out hit);
+        if (hitSomething) {
+            Debug.Log(hit.ToString());
+        } else {
+            Debug.Log("Not hitting anything");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
