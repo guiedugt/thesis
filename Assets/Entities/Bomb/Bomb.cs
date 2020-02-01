b0bceb3c62b86434cd2743aaf8d92ebe99e6d60a
GuilhermeEd <guiedugt@gmail.com>
2020-02-01 16:22:13 -0500

    Add detect bricks from bomb

---
+++ Assets/Entities/Bomb/Bomb.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] float explosionRadius = 7f;
+
+    Collider col;
+
+    void Start()
+    {
+        col = GetComponent<Collider>();
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        bool hitBrick = LayerMask.LayerToName(other.gameObject.layer) == Constants.Layers.BRICKS;
+        if (!hitBrick) return;
+
+        Explode(other.gameObject.layer);
+    }
+
+    void Explode(int layerMask)
+    {
+        int ignoredLayerMask = ~ layerMask;
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, explosionRadius, ignoredLayerMask);
+        
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Helpers/Constants.cs
@@ -0,0 +1,7 @@
+ public static class Constants
+ {
+   public class Layers
+   {
+     public const string BRICKS = "Bricks";
+   }
+ }
