ead2c8098cbe7ad330e4b099127a1ef8aea022dc
GuilhermeEd <guiedugt@gmail.com>
2020-02-02 12:57:26 -0500

    Add brick explosion

--- Assets/Entities/Bomb/Bomb.cs
+++ Assets/Entities/Bomb/Bomb.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
 public class Bomb : MonoBehaviour
 {
-    [SerializeField] float explosionRadius = 7f;
+    [SerializeField] float explosionForce = 2000f;
+    [SerializeField] float explosionRadius = 3f;
 
     Collider col;
 
     void Start()
     {
         col = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * 2f);
     }
 
     void OnDrawGizmosSelected()
@@ -29,9 +36,13 @@ public class Bomb : MonoBehaviour
 
     void Explode(int layerMask)
     {
-        int ignoredLayerMask = ~ layerMask;
-        Collider[] hitColliders = Physics.OverlapSphere(transform.position, explosionRadius, ignoredLayerMask);
-        
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, explosionRadius, 1 << layerMask);
+        Rigidbody[] rigidBodies = hitColliders.Select(t => t.gameObject.GetComponent<Rigidbody>()).ToArray();
+        foreach (Rigidbody rigidBody in rigidBodies)
+        {
+            rigidBody.isKinematic = false;
+            rigidBody.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+        }
         Destroy(gameObject);
     }
 }
