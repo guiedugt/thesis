ea085ff7d01e7bfda2a6119210249531e9c54adc
Guilherme Eduardo <guilherme@shopbonsai.ca>
2020-02-15 12:50:14 -0500

    WIP Obstacle Spawner

---
+++ Assets/Entities/Obstacle Spawner/ObstacleSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    [SerializeField] float delay = 3f;
+
+    WaitForSeconds wait;
+
+    void Start()
+    {
+        wait = new WaitForSeconds(delay);
+        StartCoroutine(SpawnCoroutine());
+    }
+
+    IEnumerator SpawnCoroutine() {
+        yield return wait;
+        Instantiate(prefab, transform.position, transform.rotation, MemoryManager.Instance.transform);
+        yield return SpawnCoroutine();
+    }
+}
