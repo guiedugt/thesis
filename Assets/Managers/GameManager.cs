c464cd2316170fbf3abf3316546c5dd46d9627ca
GuilhermeEd <guiedugt@gmail.com>
2020-03-01 17:37:42 -0500

    Add car movement

--- Assets/Managers/GameManager.cs
+++ Assets/Managers/GameManager.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
- 
+
 public class GameManager : Singleton<GameManager>
 {
     public static new Camera camera;
+    public static GameObject player;
 
     void Awake()
     {
         camera = FindObjectOfType<Camera>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 }
--- Assets/Managers/InputManager.cs
+++ Assets/Managers/InputManager.cs
@@ -14,6 +14,9 @@ public class InputManager : Singleton<InputManager>
     [SerializeField] float cameraRotation = 10f;
     [SerializeField] float cameraDisplacement = 2f;
     [SerializeField] float cameraChangeSpeed = 0.4f;
+    [SerializeField] float playerTiltDisplacement = 2f;
+    [SerializeField] float playerChangeSpeed = 0.4f;
+    [Tooltip("Used for debugging only")][Range(-1f, 1f)][SerializeField] float fakeTilt = 0f;
 
     new Camera camera;
     Vector3 initialCameraPosition;
@@ -23,14 +26,21 @@ public class InputManager : Singleton<InputManager>
     Vector3 cameraVelocity;
     float cameraAngularVelocity;
 
+    GameObject player;
+    Vector3 initialPlayerPosition;
+    Vector3 playerVelocity;
+
     void Start()
     {
-        camera = FindObjectOfType<Camera>();
+        camera = GameManager.camera;
         initialCameraPosition = camera.transform.position;
         initialCameraRotation = camera.transform.rotation;
         previousCameraPosition = initialCameraPosition;
         previousCameraRotation = initialCameraRotation;
         cameraVelocity = Vector3.zero;
+
+        player = GameManager.player;
+        initialPlayerPosition = player.transform.position;
     }
 
     void Update()
@@ -40,7 +50,7 @@ public class InputManager : Singleton<InputManager>
 
     void FixedUpdate()
     {
-        HandleCamera();
+        HandleTilt();
     }
 
     void HandleBombThrow()
@@ -60,38 +70,62 @@ public class InputManager : Singleton<InputManager>
         bombRigidbody.AddTorque(bombTorque);
     }
 
-    void HandleCamera()
+    void HandleTilt()
     {
-        float tilt = Input.acceleration.x;
+        float tilt = (Mathf.Abs(fakeTilt) >= Mathf.Epsilon) ? fakeTilt : Input.acceleration.x;
 
         bool isLeft = tilt < -maxTilt;
         bool isRight = tilt > maxTilt;
+        Vector3 positionOffsetDirection = isLeft ? Vector3.left : isRight ? Vector3.right : Vector3.zero;
 
-        Vector3 positionOffset = isLeft ?
-            Vector3.left * cameraDisplacement :
-            isRight ?
-            Vector3.right * cameraDisplacement :
-            Vector3.zero;
+        Vector3 cameraPositionOffset = positionOffsetDirection * cameraDisplacement;
+        MoveCamera(cameraPositionOffset);
 
+        float cameraRotationOffset = isLeft ? cameraRotation : isRight ? -cameraRotation : 0f;
+        RotateCamera(cameraRotationOffset);
+
+        // Move Player
+        Vector3 playerPositionOffset = positionOffsetDirection * playerTiltDisplacement;
+        MovePlayer(playerPositionOffset);
+    }
+
+    void MoveCamera(Vector3 positionOffset)
+    {
         Vector3 targetPosition = initialCameraPosition + positionOffset;
-
         if (Vector3.Distance(camera.transform.position, targetPosition) > Mathf.Epsilon)
+        {
             camera.transform.position = Vector3.SmoothDamp(
                 camera.transform.position,
                 initialCameraPosition + positionOffset,
                 ref cameraVelocity,
                 cameraChangeSpeed
             );
+        }
+    }
 
-        float rotationOffset = isLeft ? cameraRotation : isRight ? -cameraRotation : 0f;
+    void RotateCamera(float rotationOffset)
+    {
         Quaternion targetRotation = initialCameraRotation * Quaternion.Euler(0f, rotationOffset, 0f);
         float rotationDelta = Quaternion.Angle(camera.transform.rotation, targetRotation);
-        if (rotationDelta > float.Epsilon)
+        if (rotationDelta > Mathf.Epsilon)
         {
             float t = Mathf.SmoothDampAngle(rotationDelta, 0.0f, ref cameraAngularVelocity, cameraChangeSpeed);
             t = 1.0f - t / rotationDelta;
             camera.transform.rotation = Quaternion.Slerp(camera.transform.rotation, targetRotation, t);
         }
+    }
 
+    void MovePlayer(Vector3 positionOffset)
+    {
+        Vector3 targetPosition = initialPlayerPosition + positionOffset;
+        if (Vector3.Distance(player.transform.position, targetPosition) > Mathf.Epsilon)
+        {
+            player.transform.position = Vector3.SmoothDamp(
+                player.transform.position,
+                initialPlayerPosition + positionOffset,
+                ref playerVelocity,
+                playerChangeSpeed
+            );
+        }
     }
 }
