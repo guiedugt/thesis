71a13af15ad89e0709f427e9c34819b3fd95b51b
GuilhermeEd <guiedugt@gmail.com>
2020-03-05 21:19:03 -0500

    Add cannon

--- Assets/Managers/InputManager.cs
+++ Assets/Managers/InputManager.cs
@@ -8,6 +8,7 @@ public class InputManager : Singleton<InputManager>
     [SerializeField] GameObject bombPrefab;
     [SerializeField][Range(1f, 10f)] float throwPower = 7f;
     [SerializeField][Range(1f, 20f)] float touchCameraDistance = 5f;
+    [SerializeField] Transform bombOrigin;
 
     [Header("Camera")]
     [SerializeField] float maxTilt = 0.15f;
@@ -57,13 +58,14 @@ public class InputManager : Singleton<InputManager>
     {
         if (!Input.GetMouseButtonDown(0)) return;
 
-        Vector3 clickPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, touchCameraDistance);
-        Vector3 bombPosition = GameManager.camera.ScreenToWorldPoint(clickPosition);
+        Vector3 clickScreenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, touchCameraDistance);
 
-        GameObject bomb = Instantiate(bombPrefab, bombPosition, Quaternion.identity, MemoryManager.Instance.transform);
+        Vector3 clickPosition = GameManager.camera.ScreenToWorldPoint(clickScreenPosition);
+
+        GameObject bomb = Instantiate(bombPrefab, bombOrigin.position, Quaternion.identity, MemoryManager.Instance.transform);
         Rigidbody bombRigidbody = bomb.GetComponent<Rigidbody>();
 
-        Vector3 throwDirection = bombPosition - GameManager.camera.transform.position;
+        Vector3 throwDirection = clickPosition - bombOrigin.position;
         Vector3 bombTorque = new Vector3(Random.Range(0, 360f), Random.Range(0, 360f), Random.Range(0f, 360f));
 
         bombRigidbody.velocity = throwDirection * throwPower;
@@ -72,6 +74,7 @@ public class InputManager : Singleton<InputManager>
 
     void HandleTilt()
     {
+        fakeTilt = Input.GetAxis("Horizontal");
         float tilt = (Mathf.Abs(fakeTilt) >= Mathf.Epsilon) ? fakeTilt : Input.acceleration.x;
 
         bool isLeft = tilt < -maxTilt;
