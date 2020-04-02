c3e496c986ca73754e0bd57d4e4808846cc381a9
GuilhermeEd <guiedugt@gmail.com>
2020-04-01 23:23:35 -0400

    WIP Level system

--- Assets/Entities/Obstacle Spawner/ObstacleSpawner.cs
+++ Assets/Entities/Obstacle Spawner/ObstacleSpawner.cs
@@ -3,37 +3,26 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    [Header("Spawner Properties")]
-    [SerializeField] GameObject prefab;
-    [SerializeField] bool hasInitialDelay = false;
-    [SerializeField] float delay = 3f;
-
-    [Header("Obstacle Properties")]
-    [SerializeField] float minSpeed = 8f;
-    [SerializeField] float maxSpeed = 12f;
-    [SerializeField] float minZigZagSpeed = 1f;
-    [SerializeField] float maxZigZagSpeed = 5f;
-    [SerializeField] float minZigZagDistance = 0f;
-    [SerializeField] float maxZigZagDistance = 10f;
+    [SerializeField] GameObject obstaclePrefab;
 
     WaitForSeconds wait;
     WaitForSeconds oneSec = new WaitForSeconds(1);
 
     void Start()
     {
-        wait = new WaitForSeconds(delay);
-        GameManager.Instance.OnGameStart.AddListener(() => StartCoroutine(SpawnCoroutine(hasInitialDelay)));
+        wait = new WaitForSeconds(ObstacleManager.Instance.delay);
+        GameManager.Instance.OnGameStart.AddListener(() => StartCoroutine(SpawnCoroutine(ObstacleManager.Instance.hasInitialDelay)));
         GameManager.Instance.OnGameOver.AddListener(() => StopAllCoroutines());
     }
 
     IEnumerator SpawnCoroutine(bool waitForDelay = true)
     {
         if (waitForDelay) { yield return wait; }
-        GameObject obstacleGameObject = Instantiate(prefab, transform.position, transform.rotation, MemoryManager.Instance.transform);
+        GameObject obstacleGameObject = Instantiate(obstaclePrefab, transform.position, transform.rotation, MemoryManager.Instance.transform);
         Obstacle obstacle = obstacleGameObject.GetComponent<Obstacle>();
-        obstacle.speed = Random.Range(minSpeed, maxSpeed);
-        obstacle.zigZagSpeed = Random.Range(minZigZagSpeed, maxZigZagSpeed);
-        obstacle.zigZagDistance = Random.Range(minZigZagDistance, maxZigZagDistance);
+        obstacle.speed = Random.Range(ObstacleManager.Instance.minSpeed, ObstacleManager.Instance.maxSpeed);
+        obstacle.zigZagSpeed = Random.Range(ObstacleManager.Instance.minZigZagSpeed, ObstacleManager.Instance.maxZigZagSpeed);
+        obstacle.zigZagDistance = Random.Range(ObstacleManager.Instance.minZigZagDistance, ObstacleManager.Instance.maxZigZagDistance);
         yield return SpawnCoroutine();
     }
 }
