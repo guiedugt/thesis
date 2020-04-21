c5a484dc1abfbc4104a8a794732c2b00e0cbc38d
Guilherme Eduardo <guilherme@shopbonsai.ca>
2020-04-21 18:10:41 -0400

    WIP Score Panel

---
+++ Assets/UI/Score Panel/ScorePanel.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScorePanel : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI levelAmountText;
+    [SerializeField] TextMeshProUGUI levelCoinsText;
+    [SerializeField] TextMeshProUGUI brickAmountText;
+    [SerializeField] TextMeshProUGUI brickCoinsText;
+    [SerializeField] TextMeshProUGUI timeAmountText;
+    [SerializeField] TextMeshProUGUI timeCoinsText;
+    [SerializeField] TextMeshProUGUI gameCoins;
+    [SerializeField] TextMeshProUGUI totalCoins;
+
+    void Start()
+    {
+        levelAmountText.text = "x 0";
+        levelCoinsText.text = "0";
+        brickAmountText.text = "x 0";
+        brickCoinsText.text = "0";
+        timeAmountText.text = "x 0";
+        timeCoinsText.text = "0";
+        gameCoins.text = "+ 0";
+        totalCoins.text = "0";
+    }
+}
