d508eaefff67f237287ba52379d82ef753ec8dad
Guilherme Eduardo <guilherme@shopbonsai.ca>
2020-04-25 19:47:14 -0400

    Add Total Coins

---
+++ Assets/UI/Total Coins/TotalCoins.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+ 
+public class TotalCoins : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI amountText;
+
+    void Start()
+    {
+        UpdateAmount();
+    }
+
+    public void UpdateAmount()
+    {
+        float score = PlayerPrefsManager.GetTotalScore();
+        amountText.text = score.ToString("#,##0");
+    }
+}
