fc82809f7f2772f80576c0a2b6b769328a9c5f0e
Guilherme Eduardo <guilherme@shopbonsai.ca>
2020-05-03 15:46:28 -0400

    WIP Car Purchase

--- Assets/UI/CarPurchaseButton.cs
+++ Assets/UI/CarPurchaseButton.cs
@@ -1,14 +1,22 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
  
 public class CarPurchaseButton : MonoBehaviour
 {
     [SerializeField] GameObject alertInstance;
+    [SerializeField] TextMeshProUGUI amountText;
+
     Alert alert;
     
     void Start()
     {
         alert = alertInstance.GetComponent<Alert>();
         alertInstance.SetActive(false);
+    }
+
+    public void UpdateAmount(float amount)
+    {
+        amountText.text = amount.ToString("#,##0");
     }
 
     public void OpenConfirmationModal()
--- Assets/UI/CarSelectorButtonSwitch.cs
+++ Assets/UI/CarSelectorButtonSwitch.cs
@@ -5,9 +5,17 @@ public class CarSelectorButtonSwitch : MonoBehaviour
     [SerializeField] GameObject selectButton;
     [SerializeField] GameObject purchaseButton;
 
+    CarPurchaseButton purchaseButtonComponent;
+
+    void Start()
+    {
+        purchaseButtonComponent = purchaseButton.GetComponent<CarPurchaseButton>();
+    }
+
     public void Switch(SelectableCar selectedCar)
     {
         selectButton.SetActive(selectedCar.isUnlocked);
         purchaseButton.SetActive(!selectedCar.isUnlocked);
+        if (!selectedCar.isUnlocked) purchaseButtonComponent.UpdateAmount(selectedCar.cost);
     }
 }
