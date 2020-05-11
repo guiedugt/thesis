ed7b7811da87a18b2564a885f3c166599f20b83a
Guilherme Eduardo <guilherme@shopbonsai.ca>
2020-05-11 19:47:34 -0400

    Add music

---
+++ Assets/UI/Mute Button/MuteButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Toggle))]
+public class MuteButton : MonoBehaviour
+{
+    Toggle toggle;
+
+    void Start()
+    {
+        toggle = GetComponent<Toggle>();
+        toggle.onValueChanged.AddListener(Toggle);
+        toggle.isOn = AudioListener.pause;
+    }
+
+    public void Toggle(bool isMuted)
+    {
+        AudioListener.pause = isMuted;
+    }
+}
